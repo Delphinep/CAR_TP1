8c034bfe4b2c9f0048018f48bcb877f554a6e766
WebTogz <antonin.carette@gmail.com>
2015-01-28 11:34:23 +0100

    FTPServer.java : Server implementation

--- src/FTPServer/Server/FTPServer.java
+++ src/FTPServer/Server/FTPServer.java
@@ -9,11 +9,49 @@ import FTPServer.User.User;
  */
 public class FTPServer {
 	
-	private ArrayList<FTPRequest> ftp_requests;
+	public static ArrayList<FTPRequest> ftp_requests;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		try {
+			ServerSocket socket = new ServerSocket(4000);
+			
+			while(true) {
+				
+				Socket socket_communication = socket.accept();
+				FTPRequest request = new FTPRequest(socket_communication);
+				addFtpRequest(request);
+				request.run();
+				
+			}
+			
+		} 
+		catch (IOException e) {
+			System.out.println("ERROR: "+e.getMessage());
+		}
+		
+	}
+
+	/**
+	 * @return the ftp_requests
+	 */
+	public static ArrayList<FTPRequest> getFtpRequests() {
+		return ftp_requests;
+	}
+
+	/**
+	 * @param ftp_requests the ftp_requests to set
+	 */
+	public static void setFtpRequests(ArrayList<FTPRequest> ftp_requests) {
+		ftp_requests = ftp_requests;
+	}
+	
+	/**
+	 * Method to add a FTP request to the ftp_requests attribute
+	 * @param ftp_request
+	 */
+	public static void addFtpRequest(FTPRequest ftp_request) {
+		ftp_requests.add(ftp_request);
 	}
 
 }
