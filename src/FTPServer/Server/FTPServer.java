05fd61e8db8897afeb98c1f4baea11ddb036fc39
WebTogz <antonin.carette@gmail.com>
2015-01-28 11:17:59 +0100

    FTPServer.java : List of FTPRequest objects added as attribute

--- src/FTPServer/Server/FTPServer.java
+++ src/FTPServer/Server/FTPServer.java
@@ -1,11 +1,15 @@
 package FTPServer.Server;
 
+import java.util.ArrayList;
+
+import FTPServer.User.User;
+
 /**
  * Class which allows to implement a multi-thread FTP server
  */
 public class FTPServer {
 	
-	private User user;
+	private ArrayList<FTPRequest> ftp_requests;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
