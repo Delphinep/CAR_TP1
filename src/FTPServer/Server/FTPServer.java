fb6b750efa1406028ef8aae7096bf8ba84917ebc
WebTogz <antonin.carette@gmail.com>
2015-01-28 11:09:38 +0100

    FTPServer.java : user object added as attribute

--- src/FTPServer/Server/FTPServer.java
+++ src/FTPServer/Server/FTPServer.java
@@ -4,10 +4,12 @@ package FTPServer.Server;
  * Class which allows to implement a multi-thread FTP server
  */
 public class FTPServer {
+	
+	private User user;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
