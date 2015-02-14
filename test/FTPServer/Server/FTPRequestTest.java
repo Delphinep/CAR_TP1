c90a65836df6c07ec62bdb76c774f97fc312bc5c
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-14 18:35:08 +0100

    FTPRequestTest.java : Test of the request SYST done.
    The request SYST can return only one value. So one test is needed only.

--- test/FTPServer/Server/FTPRequestTest.java
+++ test/FTPServer/Server/FTPRequestTest.java
@@ -2,6 +2,9 @@ package FTPServer.Server;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 
 import util.RootFileCSV;
@@ -12,7 +15,12 @@ public class FTPRequestTest {
     @Test
     public void testProcess() {
         FTPRequest req = new FTPRequest(null,new RootFileCSV());
+        Pattern p;
+        Matcher m;
         
+        /**
+         * Resquest USER / PASS
+         */
         //Request USER with a login that exist into the account file -> OKAY
         assertTrue(req.processRequest("USER", "grutier").equals("331 : User name okay, need password.\n"));
         //Request PASS with the existing user and the good password -> OKAY
@@ -21,7 +29,6 @@ public class FTPRequestTest {
         assertTrue(req.processRequest("PASS", "nawak").equals("530 : Not logged in.\n"));
         //Request PASS with the existing user but an empty password -> FAIL
         assertTrue(req.processRequest("PASS", "").equals("530 : Not logged in.\n"));
-        
         //Request USER with a login that not exist into the account file -> OKAY
         assertTrue(req.processRequest("USER", "Je n'existe pas").equals("331 : User name okay, need password.\n"));
         //Request PASS with a non existing user -> FAIL
@@ -29,6 +36,11 @@ public class FTPRequestTest {
         //Request PASS with a non existing user and an empty password -> FAIL
         assertTrue(req.processRequest("PASS","").equals("530 : Not logged in.\n"));
 
+        /**
+         * Request SYST
+         */
+        assertTrue(req.processRequest("SYST","" ).equals("215 : Unix system.\n"));
+        
     }
 
 }
