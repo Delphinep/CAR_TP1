95bb130f9adec36665662f095113d71c916db5bd
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-17 17:08:26 +0100

    CodeMessage.java : New class add.
    It is an enumeration of all the FTP protocol codes that will be used.

---
+++ src/FTPServer/Server/CodeMessage.java
@@ -0,0 +1,10 @@
+package FTPServer.Server;
+
+public enum CodeMessage {
+    CODE_125, CODE_200, CODE_215, CODE_220,
+    CODE_221, CODE_225, CODE_226, CODE_331,
+    CODE_500, CODE_502, CODE_503, CODE_530, 
+      
+      
+
+}
