0bc9b9807a9857b013f58e8bd1c9918b1ad31283
Zangdarr <alexandre.verkyndt@gmail.com>
2015-02-17 17:20:33 +0100

    FTPRequest.java : Gestion of the SocketException.
    The problem was : after the resquest quit was received, the reading on the inputStream was shut by the socket closing procedure. To handle it I put a try/catch block with no error message because the exception is expected.

--- src/FTPServer/Server/FTPRequest.java
+++ src/FTPServer/Server/FTPRequest.java
@@ -11,6 +11,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketException;
 
 import util.RootFileCSV;
 /*
@@ -81,8 +82,16 @@ public class FTPRequest extends Thread {
              * Process requests
              */
             while(!this.finish){
-                
-                String[] request = br.readLine().split(" ");
+                String[] request;
+                try{
+                    request = br.readLine().split(" ");
+                }
+                catch (SocketException e){
+                    //The socket was closed. No problem need to be raised.
+                    
+                    //End of the thread
+                    return;
+                }
                 /*
                  * request[0] -> COMMAND NAME
                  * request[1] -> MSG
